630662e4ae32a9008d15b1d7e0955d272fd36c0d
Irene <i.oppo@cantierecreativo.net>
2018-12-17 18:19:19 +0100

    button shows label, hint and makes a fetch request

--- sidebar-button/src/index.js
+++ sidebar-button/src/index.js
@@ -1,11 +1,40 @@
 import './style.sass';
 
+const callUrl = (url, method, headers, body) => fetch(
+  url, { method, headers, body },
+)
+  .then(c => console.log(c))
+  .catch(e => console.log(e));
+
 window.DatoCmsPlugin.init((plugin) => {
   plugin.startAutoResizer();
+  const {
+    method, url, headers, label, hint,
+  } = plugin.parameters.instance;
 
   const container = document.createElement('div');
-  container.classList.add('container');
-  container.textContent = 'Hello, world!';
+  const link = document.createElement('a');
+  const p = document.createElement('p');
 
+  container.classList.add('container');
+  link.classList.add('request-button');
+
+  link.textContent = 'Clicca qui';
+  container.textContent = label;
+  p.textContent = hint;
+
+  const completeHeaders = Object.assign(
+    {},
+    headers,
+    {
+      'Access-Control-Allow-Origin': '*',
+      'content-type': 'application/json',
+    },
+  );
+
+  link.addEventListener('click', () => callUrl(
+    url, method, completeHeaders, JSON.stringify({ id: plugin.itemId }),
+  ), false);
+  container.appendChild(link);
   document.body.appendChild(container);
 });
